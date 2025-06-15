d3dadc32e411b009007351080ffbc1fc5c21e4c1
Anke Hao <ankehao10@gmail.com>
2025-06-15 11:54:08 -0700

    Fix workflow for client folder structure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/anke-hao.github.io/', // Replace with your repo name
+  base: '/',  
   build: {
     outDir: 'dist'
   }
